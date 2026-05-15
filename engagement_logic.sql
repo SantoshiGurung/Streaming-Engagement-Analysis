1fdbc3b1dc1caa0118383b4c1a302a5f75febc79
Santoshi Gurung <santoseegrg25@gmail.com>
2026-05-15 15:22:53 +0545

    Add query for genre preference by user

--- engagement_logic.sql
+++ engagement_logic.sql
@@ -14,3 +14,18 @@ JOIN watch_log AS w
     ON u.user_id = w.user_id
 GROUP BY u.user_name
 ORDER BY total_minutes_watched DESC;
+
+-- QUERY 2: GENRE PREFERENCE BY USER
+-- Goal: See which genres each user spends the most time on.
+
+SELECT 
+    users.user_name, 
+    movies.genre, 
+    SUM(watch_log.minutes_watched) AS total_min_watched
+FROM users
+JOIN watch_log 
+    ON users.user_id = watch_log.user_id
+JOIN movies 
+    ON watch_log.movie_id = movies.movie_id
+GROUP BY users.user_name, movies.genre
+ORDER BY total_min_watched DESC;
